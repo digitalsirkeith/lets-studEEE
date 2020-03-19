8d0efd9df6ae06d65e2c035e39ebd90143076f68
digitalsirkeith <kvargas@up.edu.ph>
2020-03-19 21:51:58 +0800

    Fix signup modal.

--- app/blueprints/verify.py
+++ app/blueprints/verify.py
@@ -5,11 +5,17 @@ from app.library import verify
 
 bp = Blueprint('verify', __name__, url_prefix='/verify')
 
-@bp.route('/username', methods=('GET',))
+@bp.route('/', methods=('GET',))
 def username():
     if request.method == 'GET':
         username = request.args.get('username')
+        email = request.args.get('email')
+        email_available = False
+        username_available = False
+
         if username is not None and User.query.filter(User.username == username).one_or_none() is None:
-            return verify.available(True)
-        else:
-            return verify.available(False)
+            username_available = True
+        if email is not None and User.query.filter(User.email == email).one_or_none() is None:
+            email_available = True
+
+        return verify.available(email_available, username_available)
--- app/library/verify.py
+++ app/library/verify.py
@@ -1,4 +1,4 @@
 from flask import jsonify
 
-def available(result):
-    return jsonify({'available': result})
+def available(email_available, username_available): 
+    return jsonify({'email_available': email_available, 'username_available': username_available})
